93363d54e7b4df91359e20ee9b6e84efee8cf391
Grigoriy Mirakyan <grigoriy66660@gmail.com>
2022-04-27 15:58:43 +0300

    Add_Task2+bugFix

--- Task2_Seminar/Program.cs
+++ Task2_Seminar/Program.cs
@@ -9,33 +9,27 @@ if (a == 1)
 {
      Console.WriteLine("Да это же понедельник");
 }
-else
-if (a == 2)
+else if (a == 2)
 {
      Console.WriteLine("Вторник!");
 }
-else
-if (a == 3)
+else if (a == 3)
 {
      Console.WriteLine("Среда!");
 }
-else
-if (a == 4)
+else if (a == 4)
 {
      Console.WriteLine("Четверг!");
 }
-else
-if (a == 5)
+else if (a == 5)
 {
      Console.WriteLine("Пятничка!");
 }
-else
-if (a == 6)
+else if (a == 6)
 {
      Console.WriteLine("Суббота!");
 }
-else
-if (a == 7)
+else if (a == 7)
 {
      Console.WriteLine("Воскресенье!");
 }
