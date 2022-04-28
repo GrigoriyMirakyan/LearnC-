aa4f4691adc14a7cec28725f099fe19565bcb926
Grigoriy Mirakyan <grigoriy66660@gmail.com>
2022-04-28 11:51:44 +0300

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 5, 65, 8, 45, 4, 98, 2, 7 };
+
+int n = array.Length;
+int find = 98;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
